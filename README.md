66f20a044e5d4a4d5498833cd1b3b57d858a9d74
Ståle Dahl <stalehd@gmail.com>
2020-01-18 15:15:12 +0100

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# This is the bacon
+# Bring on the bacon!
+
+Yes. The name is annoying. I write "beacon" all the time and keeps on saying
+"bacon" whenever I want to say "beacon".
+
+## Targets
+
+```
+targets/ee04_bacon
+    app=apps/bacon
+    bsp=@apache-mynewt-core/hw/bsp/telee02
+    build_profile=debug
+targets/ee04_boot
+    app=@mcuboot/boot/mynewt
+    bsp=@apache-mynewt-core/hw/bsp/telee02
+```
+
+## Building
+
+```
+newt update
+newt build ee04_boot
+newt build ee04_bacon
+```
+
+## Installing beacons
+
+```
+newt load ee04_boot
+newt build ee04_bacon
+newt create-image ee04_bacon
+newt load ee04_bacon
+```
+
+The major and minor versions for the beacons are hard coded.
--- apps/bacon/src/main.c
+++ apps/bacon/src/main.c
@@ -1,75 +1,64 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-#include <assert.h>
 #include "sysinit/sysinit.h"
 #include "os/os.h"
 #include "console/console.h"
 #include "host/ble_hs.h"
-#include "services/gap/ble_svc_gap.h"
+#include "host/ble_ibeacon.h"
 
-static const char *device_name = "the_bacon";
+const uint8_t tx_power = 0;
 
-// Measure from 1m distance
-const uint8_t measured_tx_power = 0;
-
-static void ble_app_set_addr()
+static void ble_app_set_addr(void)
 {
     ble_addr_t addr;
     int rc;
 
     rc = ble_hs_id_gen_rnd(1, &addr);
     assert(rc == 0);
-
+    console_printf("Device address= 0x%02x %02x %02x %02x %02x %02x\n", addr.val[0], addr.val[1], addr.val[2], addr.val[3], addr.val[4], addr.val[5]);
     rc = ble_hs_id_set_rnd(addr.val);
     assert(rc == 0);
 }
 
-static void ble_app_advertise()
+static int on_gap_event(struct ble_gap_event *event, void *arg)
 {
-    console_printf("Advertising!\n");
-    struct ble_gap_adv_params adv_params;
+    switch (event->type)
+    {
+    case BLE_GAP_EVENT_ADV_COMPLETE:
+        console_printf("GAP Advertising complete\n");
+        return 0;
 
-    uint8_t uuid128[16];
+    case BLE_GAP_EVENT_CONNECT:
+        console_printf("GAP connect\n");
+        return 0;
+
+    case BLE_GAP_EVENT_DISCONNECT:
+        console_printf("GAP disconnect\n");
+        return 0;
+
+    default:
+        console_printf("GAP event %d\n", event->type);
+        return 0;
+    }
+}
+
+static void ble_app_advertise(void)
+{
+    struct ble_gap_adv_params adv_params;
+    uint8_t uuid128[16] = {0x01, 0x12, 0x23, 0x34, 0x45, 0x56, 0x67, 0x78, 0x89, 0x9A, 0xAB, 0xBC, 0xCD, 0xDE, 0xEF, 0xF0};
     int rc;
 
-    /* Fill the UUID buffer with a string of 0x11 bytes. */
-    memset(uuid128, 0x12, sizeof uuid128);
+    /* Arbitrarily set the UUID to a string of 0x11 bytes. */
+    //    memset(uuid128, 0x11, sizeof uuid128);
 
     /* Major version=2; minor version=10. */
-    rc = ble_ibeacon_set_adv_data(uuid128, 2, 10, measured_tx_power);
-    if (rc != 0)
-    {
-        console_printf("Unable to set adv data\n");
-    }
+    rc = ble_ibeacon_set_adv_data(uuid128, 2, 15, tx_power);
     assert(rc == 0);
 
     /* Begin advertising. */
     adv_params = (struct ble_gap_adv_params){0};
     rc = ble_gap_adv_start(BLE_OWN_ADDR_RANDOM, NULL, BLE_HS_FOREVER,
-                           &adv_params, NULL, NULL);
-    if (rc != 0)
-    {
-        console_printf("Unable to advertise!\n");
-    }
+                           &adv_params, on_gap_event, NULL);
     assert(rc == 0);
-    console_printf("Advertising done\n");
+    console_printf("Advertising completed\n");
 }
 
 static void ble_app_on_sync(void)
@@ -79,21 +68,19 @@ static void ble_app_on_sync(void)
 
     /* Advertise indefinitely. */
     ble_app_advertise();
+
+    console_printf("Sync completed\n");
 }
 
 int main(int argc, char **argv)
 {
-    int rc;
-
     sysinit();
 
-    console_printf("Hello world, I'm the bacon!\n");
     ble_hs_cfg.sync_cb = ble_app_on_sync;
-    rc = ble_svc_gap_device_name_set(device_name);
+
+    /* As the last thing, process events from default event queue. */
     while (1)
     {
         os_eventq_run(os_eventq_dflt_get());
     }
-    assert(0);
-    return rc;
 }
